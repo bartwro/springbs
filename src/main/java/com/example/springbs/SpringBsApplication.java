230127baeb96be65aac30868809d9192c3d49307
Bartosz Starczewski <bartosz.starczewski@gmail.com>
2018-03-31 16:10:48 -0400

    first commit

---
+++ src/main/java/com/example/springbs/SpringBsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/springbs/SpringBsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
